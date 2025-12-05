ad13961b3806673064c838c044705e2b3ff24fd4
lordAjanaku <145212309+lordAjanaku@users.noreply.github.com>
2025-12-05 07:28:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  TELEGRAM_TOKEN: "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw",
-  CHAT_ID: 5710607863,
+  TELEGRAM_TOKEN: "7925332151:AAGjb0l8AGDYdOhKmk0BFp_GQN371Eb8Uzk",
+  CHAT_ID: 5349229520,
 };
 
 export default config;
